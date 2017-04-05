34023efa42e3014025625044c26a540b205858a4
Aman Dureja <dureja.aman.97@gmail.com>
2017-04-04 23:11:30 -0400

    fix function naming

--- p003.hs
+++ p003.hs
@@ -3,17 +3,19 @@
 --The prime factors of 13195 are 5, 7, 13 and 29.
 --What is the largest prime factor of the number 600851475143 ?
 --
+-- Answer: 6857
+--
 
 listOfPrimes :: [Int]
 listOfPrimes = 2 : [ x | x <- [3,5..], null (tail $ primeFactors x) ]
 
 primeFactors :: Int -> [Int]
-primeFactors n = getPrimeFactors n listOfPrimes
+primeFactors n = factors n listOfPrimes
   where
-    getPrimeFactors n (p:ps)
+    factors n (p:ps)
       | p*p > n           = [n]
-      | n `rem` p == 0    = p : getPrimeFactors (n `div` p) (p:ps)
-      | otherwise         = getPrimeFactors n ps
+      | n `rem` p == 0    = p : factors (n `div` p) (p:ps)
+      | otherwise         = factors n ps
 
 largestPrimeFactor :: Int -> Int
 largestPrimeFactor = last . primeFactors
